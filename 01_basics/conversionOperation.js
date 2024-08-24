dcdffe22083bf255ae88566c0ee7435f04c75077
vikas kz <vikasxram@gmail.com>
2024-08-24 10:17:26 +0530

    learned about prefix postfix values

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -35,8 +35,54 @@ console.log(booleanVersion) // logs true
 let num = 22
 
 let numToString = String(22)
-console.log(typeof numToString) // string
+// console.log(typeof numToString) // string
 
 
 let ok = Number(null)
-console.log
+// console.log(ok) // returns 0
+
+
+
+
+
+// ********************  OPERATIONS *******************
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+let str1 = "hello"
+let str2 = " kz"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2) // 12
+console.log(2 + "1") // 21
+console.log("1" + 2 + 2) // 122 This behavior is called type coercion
+console.log(1 + 2 + "2") // 32 
+
+
+console.log(+true) // 1
+console.log(1+true) // 2
+console.log(+"") // 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+console.log(num1)
+
+
+let gameCounter = 100
+gameCounter++   // this makes gameCounter to increase 1
+console.log(gameCounter) // logs 101
+console.log(gameCounter++) // logs 101 why?? because it is logged before adding
+console.log(gameCounter) // since 1 is added above it now becomes 102
+
+// but if I prefix the operator it'll do increment first then logs
+console.log(++gameCounter)  // returns 103
+
+// in summary:
+// Prefix returns the value after the increment.
+// Postfix returns the value before the increment.
